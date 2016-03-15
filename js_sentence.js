506e1bf76fc83afd21de13e575d2bb90b4eb2443
Cris Joya <MJoya@M-Joya.local>
2016-03-15 16:45:44 -0500

    Created Movie and Sentence JS

---
+++ js_sentence.js
@@ -0,0 +1,17 @@
+var str = "Schlitz four dollar toast godard truffaut bespoke neutra artisan pop-up. Chia keytar single-origin coffee sustainable disrupt.";
+var words = str.replace(/\./g, "").split(" ");
+
+console.log(words);
+
+function checkSixCharWords(word) {
+ if(word.length === 6){
+   return true;
+ } else {
+   return false;
+ }
+}
+
+var sixCharWords = words.filter(checkSixCharWords);
+sixCharWords = sixCharWords.length;
+
+console.log(sixCharWords);
---
+++ movies.js
@@ -0,0 +1,26 @@
+var movies = [];
+movies.push({
+  title: "Forest Gump",
+  budget: 55,
+  stars: ["Tom Hanks"]
+});
+movies.push({
+  title: "Star Wars",
+  budget: 11,
+  stars: ["Mark Hamill", "Harrison Ford"]
+});
+movies.push({
+  title: "Batman Begins",
+  budget: 150,
+  stars: ["Christian Bale", "Liam Neeson", "Michael Caine"]
+});
+movies.push({
+  title: "Titanic",
+  budget: 200,
+  stars: ["Kate Winslet", "Leonardo DiCaprio"]
+});
+movies.push({
+  title: "Inception",
+  budget: 160,
+  stars: ["Leonardo DiCaprio", "JGL"]
+});
